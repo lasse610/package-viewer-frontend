62c81eb75783b88430588a761463476473333f56
Lasse <lasse.tammela@aalto.fi>
2020-02-10 14:23:37 +0200

    fixed pagination overflow bug

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -9,27 +9,29 @@ const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
       return null;
    }
    return (
-      <nav aria-label="...">
-         <ul className="pagination">
-            <li className={currentPage === 1 ? 'page-item disabled' : 'page-item'}>
-               <button className="page-link" onClick={() => onPageChange(currentPage - 1)}>
-                  <span aria-hidden="true">&laquo;</span>
-                  <span className="sr-only">Previous</span>
-               </button>
-            </li>
-            {pages.map(page =>
-               <li key={page} className={currentPage === page ? 'page-item active' : 'page-item'}>
-                  <button className="page-link" onClick={() => onPageChange(page)}>{page}</button>
+      <div className='overflow-auto'>
+         <nav aria-label="...">
+            <ul className="pagination">
+               <li className={currentPage === 1 ? 'page-item disabled' : 'page-item'}>
+                  <button className="page-link" onClick={() => onPageChange(currentPage - 1)}>
+                     <span aria-hidden="true">&laquo;</span>
+                     <span className="sr-only">Previous</span>
+                  </button>
                </li>
-            )}
-            <li className={currentPage === pagesCount ? 'page-item disabled' : 'page-item'}>
-               <button className="page-link" onClick={() => onPageChange(currentPage + 1)}>
-                  <span aria-hidden="true">&raquo;</span>
-                  <span className="sr-only">Previous</span>
-               </button>
-            </li>
-         </ul>
-      </nav>
+               {pages.map(page =>
+                  <li key={page} className={currentPage === page ? 'page-item active' : 'page-item'}>
+                     <button className="page-link" onClick={() => onPageChange(page)}>{page}</button>
+                  </li>
+               )}
+               <li className={currentPage === pagesCount ? 'page-item disabled' : 'page-item'}>
+                  <button className="page-link" onClick={() => onPageChange(currentPage + 1)}>
+                     <span aria-hidden="true">&raquo;</span>
+                     <span className="sr-only">Previous</span>
+                  </button>
+               </li>
+            </ul>
+         </nav>
+      </div>
    );
 }
 
