1a38db8736cddfb3b8b58c0dc20c7f2cef629e75
Lasse <lasse.tammela@aalto.fi>
2020-02-10 14:11:17 +0200

    added navbar and not found page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import Packages from './components/packages';
 import PackagePage from './components/packagePage'
-
+import NavBar from './components/navBar';
+import NotFound from './components/notFound'
 function App() {
   return (
     <React.Fragment>
+      <NavBar/>
       <Switch>
          <Route path='/packages/:title' render={(props) => <PackagePage {...props} key={Math.random()}/>} />
          <Route path='/packages' component={Packages}/>
-       
+         <Route path="/not-found" component={NotFound} />
+         <Redirect exact from="/" to="/packages" />
+         <Redirect to="/not-found" />
       </Switch>
     </React.Fragment>
    
